7047cee0eada75c291d37daa77b092d77d4ef01a
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-01-26 18:30:30 +0000

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $("#like").click(() => {swipe(1);});
+    $("#hate").click(() => {swipe(-1);});
+});
+
+function swipe(increment) {
+    $.get("https://aws.random.cat/meow").then(res => {
+        let file = res.file;
+        $('#image').css("background-image", `url(${file}`);  
+    });
+}
